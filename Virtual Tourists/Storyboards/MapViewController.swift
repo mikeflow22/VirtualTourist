8707fd3dca6e2d046e6c4e94fc2a33d7f791ee87
Michael R.F <mrflowers22@uwalumni.com>
2020-03-04 06:30:29 -0800

    added a mapViewController file and scene to storyboard

---
+++ Virtual Tourists/Storyboards/MapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MapViewController.swift
+//  Virtual Tourists
+//
+//  Created by Michael Flowers on 3/4/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+}
